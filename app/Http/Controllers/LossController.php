636d8bc4337d38a8367e796e7112b7513e33f128
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-06-13 19:20:25 +0000

    add api routes

--- app/Http/Controllers/LossController.php
+++ app/Http/Controllers/LossController.php
@@ -3,25 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Loss;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LossController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        //
+        return Loss::all();
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -32,7 +34,7 @@ class LossController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Loss  $loss
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show(Loss $loss)
     {
@@ -44,7 +46,7 @@ class LossController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Loss  $loss
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, Loss $loss)
     {
@@ -55,7 +57,7 @@ class LossController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Loss  $loss
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy(Loss $loss)
     {
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\FoundLossController;
+use App\Http\Controllers\LossController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('losses', LossController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('foundlosses', FoundLossController::class);
