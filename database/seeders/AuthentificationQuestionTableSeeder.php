7685008b2088478b2054425398394a9c47222270
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-06-13 18:45:36 +0000

    add seeders

---
+++ database/seeders/AuthentificationQuestionTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AuthentificationQuestionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
